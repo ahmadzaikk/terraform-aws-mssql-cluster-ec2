bc240d47d205b981f416043ffd29f3792958628f
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2023-05-11 09:43:45 -0700

    intial commit

--- main.tf
+++ main.tf
@@ -154,7 +154,11 @@ resource "aws_ssm_document" "aws_quickstart_mssql" {
         "description": "AWS Secrets Parameter Name that has Password and User namer for the SQL Service Account.",
         "type": "String"
       },
-      
+      "SQL2017Media": {
+        "default": "https://www.kh-static-pri.net.s3.us-west-2.amazonaws.com/Install-SQLEE.ps1",
+        "description": "SQL Server 2017 installation media location",
+        "type": "String"
+      },
       "DomainDNSName": {
         "default": "kk.com",
         "description": "Fully qualified domain name (FQDN) of the forest root domain e.g. example.com",
@@ -250,9 +254,24 @@ resource "aws_ssm_document" "aws_quickstart_mssql" {
         "description": "OU the domain",
         "type": "String"
       },
+      "SQL2019Media": {
+        "default": "https://www.kh-static-pri.net.s3.us-west-2.amazonaws.com/Install-SQLEE.ps1",
+        "description": "SQL Server 2019 installation media location",
+        "type": "String"
+      },
+      "WSFCNode2PrivateIP1": {
+        "default": "10.49.69.49",
+        "description": "Secondary private IP for WSFC cluster on first WSFC Node",
+        "type": "String"
+      },
+      "SQL2016Media": {
+        "default": "https://www.kh-static-pri.net.s3.us-west-2.amazonaws.com/Install-SQLEE.ps1",
+        "description": "SQL Server 2016 installation media location",
+        "type": "String"
+      },
      
       "WSFCNode2PrivateIP2": {
-        "default": "10.49.69.38",
+        "default": "10.49.69.56",
         "description": "Secondary private IP for WSFC cluster on first WSFC Node",
         "type": "String"
       },
@@ -569,7 +588,44 @@ resource "aws_ssm_document" "aws_quickstart_mssql" {
         "name": "SqlInstallBranch",
         "action": "aws:branch"
       },
-
+      {
+        "inputs": {
+          "Parameters": {
+            "sourceInfo": "{\"path\": \"https://www.kh-static-pri.net.s3.us-west-2.amazonaws.com/DownloadSQLEE.ps1\"}",
+            "sourceType": "S3",
+            "commandLine": "./DownloadSQLEE.ps1 -SQLServerVersion {{SQLServerVersion}} -SQL2016Media {{SQL2016Media}} -SQL2017Media {{SQL2017Media}} -SQL2019Media {{SQL2019Media}}"
+          },
+          "CloudWatchOutputConfig": {
+            "CloudWatchOutputEnabled": "true",
+            "CloudWatchLogGroupName": "{{CloudwatchLogGroup}}"
+          },
+          "InstanceIds": [
+            "{{wsfcNode1InstanceId.InstanceId}}",
+            "{{wsfcNode2InstanceId.InstanceId}}"
+          ],
+          "DocumentName": "AWS-RunRemoteScript"
+        },
+        "name": "2NodeDownloadSQL",
+        "action": "aws:runCommand",
+        "onFailure": "step:sleepend"
+      },
+      {
+        "inputs": {
+          "Parameters": {
+            "sourceInfo": "{\"path\": \"https://www.kh-static-pri.net.s3.us-west-2.amazonaws.com/Install-SQLEE.ps1\"}",
+            "sourceType": "S3",
+            "commandLine": "./Install-SQLEE.ps1 -DomainNetBIOSName {{DomainNetBIOSName}} -DomainDNSName {{DomainDNSName}} -AdminSecret {{AdminSecrets}} -SQLServerVersion {{SQLServerVersion}} -SQLSecret {{SQLSecrets}} -SQLAdminGroup {{SQLAdminGroup}}"
+          },
+          "CloudWatchOutputConfig": {
+            "CloudWatchOutputEnabled": "true",
+            "CloudWatchLogGroupName": "{{CloudwatchLogGroup}}"
+          },
+          "InstanceIds": [
+            "{{wsfcNode1InstanceId.InstanceId}}",
+            "{{wsfcNode2InstanceId.InstanceId}}"
+          ],
+          "DocumentName": "AWS-RunRemoteScript"
+        },
         "name": "2NodeSQLInstallMOF",
         "action": "aws:runCommand",
         "onFailure": "step:sleepend"
